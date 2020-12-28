887f1f1e6a252bc458dc090e43053938ebd3e08f
Paul Schmiedmayer <paul.schmiedmayer@tum.de>
2020-12-28 19:24:18 +0100

    Improve the parsing or arguments and pass default arguments to the Vapor environment

--- Sources/SwiftLintBot/main.swift
+++ Sources/SwiftLintBot/main.swift
@@ -1,8 +1,12 @@
 import Vapor
 import ShellOut
 
+#if DEBUG
+var env = try Environment.detect(arguments: [CommandLine.arguments.first ?? ".", "serve", "--env", "development", "--hostname", "0.0.0.0", "--port", "8080"])
+#else
+var env = try Environment.detect(arguments: [CommandLine.arguments.first ?? ".", "serve", "--env", "production", "--hostname", "0.0.0.0", "--port", "8080"])
+#endif
 
-var env = Environment(name: "custom", arguments: ["serve"])
 try LoggingSystem.bootstrap(from: &env)
 
 let app = Application(env)
